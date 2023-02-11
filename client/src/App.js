940b073c6a83c8a08672c85bd76e21d4eb213a29
Eyerusalem Sahle <jerrysahle46@gmail.com>
2023-02-11 16:03:19 +0300

    react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
-import { BrowserRoute as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import { Provider } from "react-redux";
+import store from "./store";
 class App extends Component {
   render() {
     return (
+      // <Provider store={store}>
       <Router>
         <div className="App">
           <Navbar />
-          <Route exact path="/" Component={Landing} />
-          <div className="container">
-            <Route exact path="/register" Component={Register} />
-            <Route exact path="/login" Component={Login} />
-          </div>
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            {/* <div className="container"> */}
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+            {/* </div> */}
+          </Routes>
           <Footer />
         </div>
       </Router>
+      // </Provider>
     );
   }
 }
